f1d565e2625a4e25b11561a65efec44ba459da4f
Ian Andwati <andwatiian@gmail.com>
2023-02-17 22:35:17 +0300

    implemented download gitignore function

--- gitno/cli.py
+++ gitno/cli.py
@@ -1,5 +1,6 @@
 import click
 import os
+import requests
 
 BASE_URL = "https://api.github.com/"
 
@@ -17,3 +18,24 @@ def create_gitignore_folder():
         os.mkdirs(gitignore_folder)
 
     return gitignore_folder
+
+
+def download_gitignore_templates(output_path):
+    GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN")
+    headers = {"Authorization": f"token {GITHUB_ACCESS_TOKEN}"}
+
+    # Send a GET request to the GitHub API to retrieve a list of available gitignore templates
+    response = requests.get(f"{BASE_URL}gitignore/templates", headers=headers)
+
+    if response.status_code == 200:
+        templates = [template["name"] for template in response.json()]
+
+        for template in templates:
+            url = f"{BASE_URL}gitignore/templates/{template}"
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:
+                with open(os.path.join(output_path, f"{template}.gitignore"), "w") as f:
+                    f.write(response.text)
+    else:
+        click.echo(f"Error retrieving templates: {response.status_code}")
+        raise click.Abort()
